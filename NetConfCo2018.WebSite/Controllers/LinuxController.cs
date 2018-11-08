13146373fc485cd30d72c7fd6f90e0871b0b8b8b
Fernando <fernando.sonego@gmail.com>
2018-11-08 12:24:54 -0300

    Nuevo controllador

---
+++ NetConfCo2018.WebSite/Controllers/LinuxController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetConfCo2018.WebSite.Models;
+
+namespace NetConfCo2018.WebSite.Controllers
+{
+    public class LinuxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
